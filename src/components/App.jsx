a63af0cd7cecef946f9a0ac0928b542b000bd4ac
Andrii-Hr <andriihryb@ukr.net>
2022-12-14 15:59:16 +0200

    add markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
+import React, { useEffect, useState } from 'react';
+import Section from './Section/Section';
+import Form from './Form/Form';
+import Contacts from './Contacts/Contacts';
+import Filter from './Filter/Filter';
+
+
+export default function App() {
+  const [contacts, setContacts] = useState(() => JSON.parse(localStorage.getItem('contacts')) ?? [])
+  const [filter, setFilter] = useState('')
+
+useEffect(() => {
+localStorage.setItem('contacts', JSON.stringify(contacts))
+}, [contacts])
+
+
+  const onFormSubmit = data => {
+    if (checkName(data)) {
+      alert(`${data.name} is already in contracts.`);
+      return;
+    }
+
+    setContacts([data, ...contacts],
+    );
+  };
+  const onChange = e => {
+   setFilter(e.currentTarget.value)    
+  };
+
+  const onFilter = () => {
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(el =>
+      el.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const checkName = data => {
+    return contacts.some(el => el.name.toLowerCase() === data.name.toLowerCase());
+  };
+
+  const onContactDelete = evt => setContacts(contacts.filter(el => el.id !== evt.target.id),
+    );
+   
+ 
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1>Phonebook</h1>
+      <Form submit={onFormSubmit} />
+      <Section title="Contacts">
+        <Filter filter={filter} change={onChange} />
+        <Contacts
+          contacts={onFilter()}
+          onContactDelete={onContactDelete}
+        />
+      </Section>
+    </>
   );
-};
+}
